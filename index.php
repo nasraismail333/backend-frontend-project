1bcccf4296b7df79a122d72120e56815a499832f
nasraismail333 <143229573+nasraismail333@users.noreply.github.com>
2023-09-27 00:54:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ if(empty($firstName)){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <title>nasra</title>
+    <title>nasra ismail</title>
 </head>
 <body>
     
